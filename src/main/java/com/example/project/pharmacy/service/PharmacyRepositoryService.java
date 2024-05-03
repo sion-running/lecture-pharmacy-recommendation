6286e61a6eaf0d51d5e33b2065d1e813a0c93dee
sion-running <staythismoment.sion@gmail.com>
2024-05-03 15:55:55 +0900

    테스트코드 저장

---
+++ src/main/java/com/example/project/pharmacy/service/PharmacyRepositoryService.java
@@ -0,0 +1,41 @@
+package com.example.project.pharmacy.service;
+
+import com.example.project.pharmacy.entity.Pharmacy;
+import com.example.project.pharmacy.repository.PharmacyRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Objects;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PharmacyRepositoryService {
+    private final PharmacyRepository pharmacyRepository;
+
+    @Transactional
+    public void updateAddress(Long id, String address) {
+        Pharmacy entity = pharmacyRepository.findById(id).orElse(null);
+
+        if (Objects.isNull(entity)) {
+            log.error("[PharmacyRepositoryService updateAddress] no found id: {}", id);
+            return;
+        }
+
+        entity.changePharmacyAddress(address);
+    }
+
+    // dirty checking을 테스트 해보기 위해 transactional 어노테이션 삭제
+    public void updateAddressWithoutTransactional(Long id, String address) {
+        Pharmacy entity = pharmacyRepository.findById(id).orElse(null);
+
+        if (Objects.isNull(entity)) {
+            log.error("[PharmacyRepositoryService updateAddress] no found id: {}", id);
+            return;
+        }
+
+        entity.changePharmacyAddress(address);
+    }
+}
